3a8d7eb174333670585c44aee5f0fbc5767b0163
yamuna <yamunayy111@gmail.com>
2025-10-31 08:37:48 +0530

    Refactor User model export syntax

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,13 @@
+// backend/models/users.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true },
-  loginCount: { type: Number, default: 0 },
 });
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
+
